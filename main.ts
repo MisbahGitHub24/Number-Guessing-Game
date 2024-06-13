b05257beb94ed21ebdd3ab83f2d2dce27be1764a
Misbah Sarwar <160399812+MisbahGitHub24@users.noreply.github.com>
2024-06-13 22:49:47 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,18 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+const computerGeneratedNumber = Math.floor(Math.random() * 10 + 1);
+
+const answer = await inquirer.prompt([
+  {
+    name: "userGuessedNumber",
+    type: "number",
+    message: "Guess any number between 1 to 10",
+  },
+]);
+if (answer.userGuessedNumber === computerGeneratedNumber) {
+  console.log("Congratualations!!!! You win");
+} else {
+  console.log("You loss....Better Luck Next Time! ");
+}
+
